56d1239e295904eb531f5cfa7c4d5a757949db9f
doitles <uklelai@gmail.com>
2016-01-21 01:23:46 -0800

    Update util.cpp

--- asg2/util.cpp
+++ asg2/util.cpp
@@ -1,96 +1,63 @@
-// $Id: util.h,v 1.12 2016-01-14 16:16:52-08 - - $
+// $Id: util.cpp,v 1.11 2016-01-13 16:21:53-08 - - $
 
-// util -
-//    A utility class to provide various services not conveniently
-//    included in other modules.
+#include <cstdlib>
+#include <unistd.h>
 
-#ifndef __UTIL_H__
-#define __UTIL_H__
-
-#include <iostream>
-#include <stdexcept>
-#include <string>
-#include <vector>
 using namespace std;
 
-// Convenient type using to allow brevity of code elsewhere.
+#include "util.h"
+#include "debug.h"
 
-template <typename iterator>
-using range_type = pair<iterator,iterator>;
+int exit_status::status = EXIT_SUCCESS;
+static string execname_string;
 
-using wordvec = vector<string>;
-using word_range = range_type<decltype(declval<wordvec>().cbegin())>;
+void exit_status::set (int new_status) {
+   status = new_status;
+}
 
-// setexecname -
-//    Sets the static string to be used as an execname.
-// execname -
-//    Returns the basename of the executable image, which is used in
-//    printing error messags.
+int exit_status::get() {
+   return status;
+}
 
-void execname (const string&);
-string& execname();
+void execname (const string& name) {
+   execname_string =  name.substr (name.rfind ('/') + 1);
+   DEBUGF ('u', execname_string);
+}
 
-// want_echo -
-//    We want to echo all of cin to cout if either cin or cout
-//    is not a tty.  This helps make batch processing easier by
-//    making cout look like a terminal session trace.
+string& execname() {
+   return execname_string;
+}
 
-bool want_echo();
-
-// exit_status -
-//    A static class for maintaining the exit status.  The default
-//    status is EXIT_SUCCESS (0), but can be set to another value,
-//    such as EXIT_FAILURE (1) to indicate that error messages have
-//    been printed.
-
-class exit_status {
-   private:
-      static int status;
-   public:
-      static void set (int);
-      static int get();
-};
+bool want_echo() {
+   constexpr int CIN_FD {0};
+   constexpr int COUT_FD {1};
+   bool cin_is_not_a_tty = not isatty (CIN_FD);
+   bool cout_is_not_a_tty = not isatty (COUT_FD);
+   DEBUGF ('u', "cin_is_not_a_tty = " << cin_is_not_a_tty
+          << ", cout_is_not_a_tty = " << cout_is_not_a_tty);
+   return cin_is_not_a_tty or cout_is_not_a_tty;
+}
 
 
-// split -
-//    Split a string into a wordvec (as defined above).  Any sequence
-//    of chars in the delimiter string is used as a separator.  To
-//    Split a pathname, use "/".  To split a shell command, use " ".
+wordvec split (const string& line, const string& delimiters) {
+   wordvec words;
+   size_t end = 0;
 
-wordvec split (const string& line, const string& delimiter);
-
-// complain -
-//    Used for starting error messages.  Sets the exit status to
-//    EXIT_FAILURE, writes the program name to cerr, and then
-//    returns the cerr ostream.  Example:
-//       complain() << filename << ": some problem" << endl;
-
-ostream& complain();
-
-// operator<< (vector) -
-//    An overloaded template operator which allows vectors to be
-//    printed out as a single operator, each element separated from
-//    the next with spaces.  The item_t must have an output operator
-//    defined for it.
-
-template <typename item_t>
-ostream& operator<< (ostream& out, const vector<item_t>& vec) {
-   string space = "";
-   for (const auto& item: vec) {
-      out << space << item;
-      space = " ";
+   // Loop over the string, splitting out words, and for each word
+   // thus found, append it to the output wordvec.
+   for (;;) {
+      size_t start = line.find_first_not_of (delimiters, end);
+      if (start == string::npos) break;
+      end = line.find_first_of (delimiters, start);
+      words.push_back (line.substr (start, end - start));
    }
-   return out;
+   DEBUGF ('u', words);
+   return words;
 }
 
-template <typename iterator>
-ostream& operator<< (ostream& out, range_type<iterator> range) {
-   for (auto itor = range.first; itor != range.second; ++itor) {
-      if (itor != range.first) out << " ";
-      out << *itor;
-   }
-   return out;
+ostream& complain() {
+   exit_status::set (EXIT_FAILURE);
+   cerr << execname() << ": ";
+   return cerr;
 }
-
-#endif
 
